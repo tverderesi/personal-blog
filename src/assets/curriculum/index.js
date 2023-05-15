ea705494bf9e150acc406f53cac4b7f9a4069345
tverderesi <tverderesi@gmail.com>
2023-05-15 14:33:23 -0300

    updated curriculum

---
+++ src/assets/curriculum/index.js
@@ -0,0 +1,7 @@
+import aboutMe from './aboutMe.json';
+import contactInfo from './contactInfo.json';
+import downloadLinks from './downloadLinks.json';
+import strengths from './strengths.json';
+import techStack from './techStack.json';
+
+export { aboutMe, contactInfo, downloadLinks, strengths, techStack };
