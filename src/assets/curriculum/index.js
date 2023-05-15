104566a2a2cf39cbf1b4df42cf9355df313c9a6f
tverderesi <tverderesi@gmail.com>
2023-05-15 16:23:18 -0300

    finish curriculum

--- src/assets/curriculum/index.js
+++ src/assets/curriculum/index.js
@@ -1,8 +1,11 @@
 import aboutMe from './aboutMe.json';
 import contactInfo from './contactInfo.json';
+import courses from './courses.json';
 import downloadLinks from './downloadLinks.json';
+import education from './education.json';
+import languages from './languages.json';
 import strengths from './strengths.json';
 import techStack from './techStack.json';
 import workExperience from './workExperience.json';
 
-export { aboutMe, contactInfo, downloadLinks, strengths, techStack, workExperience };
+export { aboutMe, contactInfo, courses, downloadLinks, education, languages, strengths, techStack, workExperience };
