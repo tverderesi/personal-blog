d76945a18a914ba0cc6fe3dced5d6c7c8a56d381
tverderesi <tverderesi@gmail.com>
2023-05-15 15:23:54 -0300

    updated workXP layout

--- src/assets/curriculum/index.js
+++ src/assets/curriculum/index.js
@@ -3,5 +3,6 @@ import contactInfo from './contactInfo.json';
 import downloadLinks from './downloadLinks.json';
 import strengths from './strengths.json';
 import techStack from './techStack.json';
+import workExperience from './workExperience.json';
 
-export { aboutMe, contactInfo, downloadLinks, strengths, techStack };
+export { aboutMe, contactInfo, downloadLinks, strengths, techStack, workExperience };
